e0ba555b3da745b916bb79a759e1143bedeead54
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-04 14:26:45 +0430

    Implement find users list

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -4,9 +4,12 @@ const userModel = require('../models/userModel')
 
 const usersList = async (req, res, next) => {
     try {
+        const users = await userModel.find({})
+        const countOfUserList = await userModel.count()
         res.status(200).send({
             successes: true,
-            message: "List of users"
+            message: `The users list has ${countOfUserList} user`,
+            users
         })
     } catch (error) {
         next(error)
